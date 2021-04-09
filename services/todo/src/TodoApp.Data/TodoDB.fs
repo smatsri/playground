2fbf16805be8585dc2019f8d1fd26d83b24812ba
Shai <smatsri@gmail.com>
2021-04-09 09:59:11 +0300

    .

--- services/todo/src/TodoApp.Data/TodoDB.fs
+++ services/todo/src/TodoApp.Data/TodoDB.fs
@@ -1,9 +1,11 @@
 ﻿namespace TodoApp.Data
 open TodoApp.Domain
 open Microsoft.EntityFrameworkCore
+open System
 
-type TodoDB(options) =
-  inherit DbContext(options)
+
+type TodoDB(configure: Action<DbContextOptionsBuilder>) =
+  inherit DbContext()
 
   [<DefaultValue>]
   val mutable todoItems:DbSet<TodoItem>
@@ -11,3 +13,6 @@ type TodoDB(options) =
   member x.TodoItems  
     with get () = x.todoItems
     and set v = x.todoItems <- v
+
+  override x.OnConfiguring (options:DbContextOptionsBuilder) = 
+    configure.Invoke(options)
---
+++ services/todo/src/TodoApp.Migrations/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TodoApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApp.Migrations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
